140f1c0a6d6c014bda32e05633d4349ef0db3d30
Hong Yuan <hy8888@gmail.com>
2014-09-28 21:57:19 -0700

    Fixing code format.

--- src/main/java/com/lockmarker/LockMarkerService.java
+++ src/main/java/com/lockmarker/LockMarkerService.java
@@ -15,40 +15,41 @@ import com.yammer.dropwizard.config.Environment;
 
 public class LockMarkerService extends Service<MessagingConfiguration> {
 
-	public static void main(String[] args) throws Exception {
-		Injector injector = Guice.createInjector(new AbstractModule() {
-			@Override
-			protected void configure() {
-				// default dispatcher is NopMessagingDispatcher. To override it, 
-				// pass java command line arg -Ddispatcher=<NewDispatcher>
-				// e.g. java -Ddispatcher=Rabbit -jar msgas-0.0.1.jar server msgas.yml
-				String dispatcherClassName = System.getProperty("dispatcher");
-				Class<? extends MessagingDispatcher> dispatcherClass = RabbitMQDispatcher.class;
-				if ((dispatcherClassName != null) && 
-					(dispatcherClassName.indexOf("Rabbit") != -1)) {
-					dispatcherClass = RabbitMQDispatcher.class;
-				}
-				bind(MessagingDispatcher.class).to(dispatcherClass);
-			}
-		});
-		injector.getInstance(LockMarkerService.class).run(args);
-	}
+    public static void main(String[] args) throws Exception {
+        Injector injector = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                // default dispatcher is NopMessagingDispatcher. To override it,
+                // pass java command line arg -Ddispatcher=<NewDispatcher>
+                // e.g. java -Ddispatcher=Rabbit -jar msgas-0.0.1.jar server
+                // msgas.yml
+                String dispatcherClassName = System.getProperty("dispatcher");
+                Class<? extends MessagingDispatcher> dispatcherClass = RabbitMQDispatcher.class;
+                if ((dispatcherClassName != null)
+                        && (dispatcherClassName.indexOf("Rabbit") != -1)) {
+                    dispatcherClass = RabbitMQDispatcher.class;
+                }
+                bind(MessagingDispatcher.class).to(dispatcherClass);
+            }
+        });
+        injector.getInstance(LockMarkerService.class).run(args);
+    }
 
-	@Inject
-	private LockMarkerService(MessagingDispatcher dispatcher) {
-		super("LockMarker Service");
-	}
+    @Inject
+    private LockMarkerService(MessagingDispatcher dispatcher) {
+        super("LockMarker Service");
+    }
 
-	@Override
-	protected void initialize(MessagingConfiguration configuration,
-			                  Environment environment) {
-		try {
-			final String template = configuration.getTemplate();
-			environment.addHealthCheck(new TemplateHealthCheck(template));
+    @Override
+    protected void initialize(MessagingConfiguration configuration,
+            Environment environment) {
+        try {
+            final String template = configuration.getTemplate();
+            environment.addHealthCheck(new TemplateHealthCheck(template));
             environment.addResource(new LockMarkerResource());
-			environment.addTask(new ServiceShutdownTask());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            environment.addTask(new ServiceShutdownTask());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
